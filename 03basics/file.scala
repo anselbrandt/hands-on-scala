d1947c28635eab46df27ce01406282828167262f
anselbrandt <ansel@anselbrandt.com>
2021-03-03 17:37:57 -0500

    Add file

---
+++ 03basics/file.scala
@@ -0,0 +1,33 @@
+import java.io.{BufferedWriter, BufferedReader}
+import java.nio.file.{Files, Paths}
+
+object file extends App {
+
+  def withFileWriter[T](
+      fileName: String
+  )(handler: BufferedWriter => T) = {
+    val output =
+      Files.newBufferedWriter(Paths.get(fileName))
+    try handler(output)
+    finally output.close()
+  }
+
+  def withFileReader[T](
+      fileName: String
+  )(handler: BufferedReader => T) = {
+    val input =
+      Files.newBufferedReader(Paths.get(fileName))
+    try handler(input)
+    finally input.close()
+  }
+
+  withFileWriter("File.txt") { writer =>
+    writer.write("Hello\n"); writer.write("World!")
+  }
+
+  val result = withFileReader("File.txt") { reader =>
+    reader.readLine() + "\n" + reader.readLine()
+  }
+
+  println(result)
+}
