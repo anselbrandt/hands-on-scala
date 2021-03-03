7542f3ccf071e668801e1d6af4cd2ba64eeafe48
anselbrandt <ansel@anselbrandt.com>
2021-03-03 12:44:55 -0500

    Update tree

--- 03basics/tree.scala
+++ 03basics/tree.scala
@@ -1,26 +1,22 @@
 object test extends App {
 
-  class Node(val id: Int, val parent: Option[Int])
+  class Node(val id: Int, val parent: Option[Int], val msg: String)
 
   val nodes = Array(
-    new Node(4, Some(3)),
-    new Node(5, Some(4)),
-    new Node(10, Some(9)),
-    new Node(1, None),
-    new Node(2, Some(1)),
-    new Node(3, Some(2)),
-    new Node(6, Some(5)),
-    new Node(7, Some(6)),
-    new Node(8, Some(7)),
-    new Node(9, Some(8))
+    new Node(0, None, "Hello"),
+    new Node(1, Some(0), "World"),
+    new Node(2, None, "I am Cow"),
+    new Node(3, Some(2), "Hear me moo"),
+    new Node(4, Some(2), "Here I stand"),
+    new Node(5, Some(2), "I am Cow"),
+    new Node(6, Some(5), "Here me moo, moo")
   )
 
   def printNodes(nodes: Array[Node]) = {
 
     def printNode(parent: Option[Int], depth: Int): Unit = {
       for (node <- nodes if node.parent == parent) {
-        val indent = "  " * depth
-        println(s"${indent}${node.id} parent ${parent} depth $depth ")
+        println(s"${node.id} parent ${parent} depth $depth")
         printNode(Some(node.id), depth + 1)
       }
     }
@@ -30,10 +26,3 @@ object test extends App {
 
   printNodes(nodes)
 }
-
-/*
-
-https://www.youtube.com/watch?v=aPQY__2H3tE
-https://www.geeksforgeeks.org/dynamic-programming-trees-set-2/
-
- */
